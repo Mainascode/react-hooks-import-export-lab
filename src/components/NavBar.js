f70d3618adfb46a26ca0d5787091b5f9a1d11a4b
Maina <smurkcarter855@gmail.com>
2024-11-06 15:49:36 +0300

    origin

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,19 @@
 import React from "react";
 
+
 function NavBar() {
   return (
+    <div>
     <nav>
       <a href="#home">I'm a link!</a>
+      
     </nav>
+    <NavBar/>
+  </div>
+   
   );
 }
+
+
+
+export default NavBar;
--- src/components/data/user.js
+++ src/components/data/user.js
@@ -1,6 +1,12 @@
+import About from "../About";
+import Home from "../Home";
+import NavBar from "../NavBar";
+
 const username = "Liza";
 const city = "New York";
 
 const image = "https://i.imgur.com/mV8PQxj.gif";
 
 // export all three variables as named exports
+
+export {username, city, image}
